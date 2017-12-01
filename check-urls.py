c138c42c7a2a23801ea3d9f68a98a1b8eec6f191
FP <f.pestre@valabre.com>
2017-12-01 13:21:27 +0000

    remove some comments (stackoverflow) references

--- check-urls.py
+++ check-urls.py
@@ -9,7 +9,6 @@ def get_server_status_code(url):
     Download just the header of a URL and
     return the server's status code.
     """
-    # http://stackoverflow.com/questions/1140661
     host, path = urlparse.urlparse(url)[1:3]    # elems [1] and [2]
     print "host : ", host, " | path : ", path
     try:
@@ -26,7 +25,6 @@ def check_url(url):
     Check if a URL exists without downloading the whole file.
     We only check the URL header.
     """
-    # see also http://stackoverflow.com/questions/2924422
     good_codes = [httplib.OK, httplib.FOUND, httplib.MOVED_PERMANENTLY]
     return get_server_status_code(url) in good_codes
 
